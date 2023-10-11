f414d593121b64a351bd624e2c85c823aaa30564
Shreya Singh <shreyasingh200297.ss@gmail.com>
2023-10-11 11:17:32 -0400

    project setup

---
+++ NYCSchools/NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSchoolsApp.swift
+//  NYCSchools
+//
+//  Created by Aerologix Aerologix on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
